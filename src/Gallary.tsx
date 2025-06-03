cad4fd09ee47dbba41754baa0d19a90984f44927
Varadh <varadkumar4879@gmail.com>
2025-06-03 20:20:36 +0530

    created gallery component

---
+++ src/Gallary.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Gallary: React.FC = () => {
+    return (
+        <div>
+            <h1>Gallery</h1>
+            <p>Welcome to the gallery page. Add your images here!</p>
+        </div>
+    );
+};
+
+export default Gallary;
